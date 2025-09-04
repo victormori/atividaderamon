a9a177332acd56086d32e266cf3171b58792ada7
victormori <victordaab@gmail.com>
2025-09-04 11:07:38 -0300

    atividades de lm

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PrincipalController;
+
+// rota para Missão, Chef
+Route::get('/chef', [PrincipalController::class, 'chef']);
+
+// rota para Meu Primeiro Cartão de Visita Online
+Route::get('/card', [PrincipalController::class, 'card']);
+
+// rota para Meu Primeiro Perfil Online
+Route::get('/profile', [PrincipalController::class, 'profile']);
